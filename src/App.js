e28176b8902bae01da8887f043937bc3b321dcf8
darthrighteous <ogunniyiabduljalil@yahoo.com>
2019-04-14 13:44:44 +0100

    set up routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Header from './components/header'
+import Pages from './pages';
+
+const { HomePage, ProfilePage, FourZeroFour } = Pages
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/my-profile' component ={ProfilePage} />
+        <Route component={FourZeroFour}/>
+      </Switch>
+    </Fragment>
+  </Router>
+)
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import home from './home';
+import profile from './profile';
+import notfound from './notfound';
+
+
+export default {
+  HomePage: home,
+  ProfilePage: profile,
+  FourZeroFour: notfound
+}
